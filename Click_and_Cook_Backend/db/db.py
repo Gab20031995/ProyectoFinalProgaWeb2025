f80775a3191f8978a81137c41f5f47c3160f916c
Jean Rabbt <jean.rabbat@ulead.ac.cr>
2025-08-16 16:25:23 -0600

    Backend leads agregado

--- Click_and_Cook_Backend/db/db.py
+++ Click_and_Cook_Backend/db/db.py
@@ -4,10 +4,10 @@ from mysql.connector import errorcode
 
 # Tus credenciales de la base de datos
 DB_CONFIG = {
-    'host': "127.0.0.1",
-    'port': "3307",
+    'host': "localhost",
+    'port': "3306",
     'user': "root",
-    'password': "123Queso.",
+    'password': "0818Jeank*",
     'database': "recetas_db"
 }
 
